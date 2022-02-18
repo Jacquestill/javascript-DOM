7bee584f2cd4c734a2cb943152ab7013eb68dc82
Jacquestill <89329719+Jacquestill@users.noreply.github.com>
2022-02-18 20:11:46 +0900

    Exercises completed

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-const heading = document.querySelector('h1');
+'use strict';
 
-console.log('Hello World');
+// const heading = document.querySelector('h1');
+// const thirdItem = document.querySelector('ul').lastElementChild;
+//
+//
+// thirdItem.innerHTML = 'Jacques';
+//
+// // ---------- Selecting by Tag name ----------
+// const tag = document.getElementsByTagName('li');
+// tag[2].style.color = 'purple';
+// console.log(tag[0]);
+// console.log(tag.length);
+//
+// // ---------- Selecting by Class name ----------
+//
+// const classBtn = document.getElementsByClassName('btn');
+// console.log(classBtn);
+//
+// // ---------- Selecting by ID name ----------
+//
+// const id = document.getElementById('title');
+// id.style.color = 'green';
+// id.innerHTML = 'Bye Bye';
+//
+// // ---------- Selecting by querySelector ----------
+//
+// // Element: h1
+// const elementQuery = document.querySelector('h1');
+//
+// // ID: #title
+// const idQuery = document.querySelector('#title');
+//
+// // Class: .item
+// const classQuery = document.querySelector('.item');
+//
+// // Attributes
+// const attriQuery = document.querySelector('li a');
+//
+// // Combining
+// const comb = document.querySelector('li.item');
+//
+// const google = document.querySelector('.item a');
+// google.style.color = 'yellow';
+
+const btn = document.querySelector('.btn');
+
+btn.style.backgroundColor = 'yellow';
